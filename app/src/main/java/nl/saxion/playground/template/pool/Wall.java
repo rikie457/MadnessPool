e46653a5144a8c942cb0647a26ed0b1860d1d4fa
bblekkink <466872@student.saxion.nl>
2019-06-13 10:02:56 +0200

    updated Wall class

--- app/src/main/java/nl/saxion/playground/template/pool/Wall.java
+++ app/src/main/java/nl/saxion/playground/template/pool/Wall.java
@@ -1,19 +1,55 @@
 package nl.saxion.playground.template.pool;
 
+import android.view.MotionEvent;
+
 import java.util.ArrayList;
 
 import nl.saxion.playground.template.lib.Entity;
 import nl.saxion.playground.template.pool.balls.Ball;
 
 public class Wall extends Entity {
-    private float x;
-    private float y;
-    private double height;
-    private double width;
+    private double x, y;
+    private double width, height;
+    private int wallId;
+
+    private static int wallIdCreate = 1;
 
     ArrayList<Ball> balls;
+    ArrayList<Hole> holes;
 
-    public Wall (ArrayList<Ball> balls) {
+    public Wall (ArrayList<Ball> balls, ArrayList<Hole> holes) {
         this.balls = balls;
+        this.holes = holes;
+        this.wallId = this.wallIdCreate;
+
+        this.wallIdCreate++;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getWallId() {
+        return wallId;
     }
 }
